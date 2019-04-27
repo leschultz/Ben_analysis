df92016f35cc837640d09022a27fdd9bc944421b
leschultz <laneenriqueschultz@gmail.com>
2019-04-27 13:17:13 -0500

    file downloader

--- python/filecopier.py
+++ python/filecopier.py
@@ -1,129 +1,20 @@
-from shutil import copy, copytree
-
+from shutil import copyfile
+from os.path import join
 import tarfile
 import sys
 import os
 
 
-# The name of important files for each job
-trajdotlammpstrj = 'traj.lammpstrj'
-testdotout = 'test.out'
-depdotin = 'dep.in'
-compressed = 'outputs.tar.gz'
-
-
-def filecopy(copyname, filelist, copypath, savepath):
-    '''
-    Copy the file if it exists in path.
-
-    inputs:
-        copyname = The name of the file of interest
-        filelist = The list of files available
-        copypath = The path from where the file is copied from
-        savepath = The path to save the file
-
-    outputs:
-        A copied file in a directory
-    '''
-
-    # Grab the output if the file exists
-    if (copyname in filelist):
-        exist = copyname
-        filename = os.path.join(copypath, copyname)
-
-        # Create the path to work in
-        if not os.path.exists(savepath):
-            os.makedirs(savepath)
-
-        copy(filename, savepath)
-
-
-def copydata(item, outdir):
-    '''
-    Copy the data.
-
-    inputs:
-        item = The output from a os.walk generator
-        outdir = The name for the export path
-
-    outputs:
-        A collection of data files
-    '''
-
-    if ('job' in item[0]) & ('minimization' not in item[0]):
-
-        # Were parsed data will be stored
-        data = []
-
-        # Create a name from the path
-        name = item[0].split('/')
-        name = name[-4:]
-        name = os.path.join(*name)
-
-        # Print status
-        print('Copying data: '+name)
-
-        savepath = os.path.join(*[outdir, name])
-
-        # Grab the output archive file that contains run system data
-        if (compressed in item[-1]) & (depdotin in item[-1]):
-            filename = os.path.join(*[item[0], compressed])
-            inputfile = os.path.join(*[item[0], depdotin])
-
-            # Open the archive
-            archive = tarfile.open(filename, 'r')
-
-            # Iterate for each file in the archive
-            for member in archive.getmembers():
-
-                # Open the file containing system data
-                if testdotout in str(member):
-                    member = archive.extractfile(member)
-                    content = member.read()
-
-                # Open the file containing the trajectory data
-                if trajdotlammpstrj in str(member):
-                    member = archive.extractfile(member)
-                    traj = member.read()
-
-            # Create the path to work in
-            if not os.path.exists(savepath):
-                os.makedirs(savepath)
-
-            # Save the information up one directory
-            copy(inputfile, savepath)
-            open(os.path.join(savepath, testdotout), 'wb').write(content)
-            open(os.path.join(savepath, trajdotlammpstrj), 'wb').write(traj)
-
-        # Grab the files if they exist
-        else:
-
-            if testdotout in item[-1]:
-                filecopy(testdotout, item[-1], item[0], savepath)
-            if depdotin in item[-1]:
-                filecopy(depdotin, item[-1], item[0], savepath)
-            if trajdotlammpstrj in item[-1]:
-                filecopy(trajdotlammpstrj, item[-1], item[0], savepath)
-
-    if 'minimization' in item[0]:
-
-        # Create a name from the path
-        name = item[0].split('/')
-        name = name[-5:]
-        name = os.path.join(*name)
-
-        savepath = os.path.join(*[outdir, name])
-
-        copytree(item[0], savepath)
-
-
-def jobiterator(path, outdir):
+def iterator(path, outdir, compressed, folders, files, depth):
     '''
     Search for all possible files to copy for Tg analysis.
 
     inputs:
         path = The path with all the runs
         outdir = The export path
+        compressed = List of compressed files
+        files = List of needed files
+        depth = The depth for directory naming convention
 
     outputs:
         A collection of data files
@@ -131,11 +22,69 @@ def jobiterator(path, outdir):
 
     # Look for all directories as generator object
     paths = os.walk(path)
+    extensions = [i.split('.')[-1] for i in files]
 
     # Loop for each path
     for item in paths:
-        copydata(item, outdir)
+        for tar in compressed:
+            if tar not in item[2]:
+                continue
 
+            archive = tarfile.open(join(item[0], tar), 'r')
+            for member in archive.getmembers():
+                if member.name not in files:
+                    continue
+
+                name = item[0].split('/')
+                name = name[-depth:]
+                name = join(outdir, *name)
+
+                # Create the path
+                if not os.path.exists(name):
+                    os.makedirs(name)
+
+                name = join(name, member.name)
+
+                print('Creating: '+name)
+                content = archive.extractfile(member).read()
+                open(name, 'wb').write(content)
+
+        for folder in folders:
+            if folder not in item[0]:
+                continue
+
+            for f in item[2]:
+                if f not in files:
+                    continue
+
+                name = item[0].split('/')
+                name = name[-depth-1:]+item[1]
+                name = join(outdir, *name)
+
+                # Create the path
+                if not os.path.exists(name):
+                    os.makedirs(name)
+
+                name = join(name, f)
+
+                print('Creating: '+name)
+                copyfile(join(item[0], f), name)
+
+# The name of important files for each job
+compressed = [
+              'inputs.tar.gz',
+              'outputs.tar.gz'
+              ]
+folders = [
+           '100K_Structure_minimization'
+           ]
+files = [
+         'traj.lammpstrj',
+         'test.out',
+         './dep.in',
+         'finaltraj.lammpstrj',
+         '100k_minimize_template.in'
+         ]
 
 # Download data into current directory
-jobiterator(sys.argv[1], sys.argv[2])
+iterator(sys.argv[1], sys.argv[2], compressed, folders, files, 5)
