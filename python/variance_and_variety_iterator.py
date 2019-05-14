9a89a22d0f0c4e87078c7139cc9a2ba13d0e32a7
leschultz <laneenriqueschultz@gmail.com>
2019-05-14 15:31:37 -0500

    Added an iterator for variety and variance.

---
+++ python/variance_and_variety_iterator.py
@@ -0,0 +1,41 @@
+from scipy.signal import argrelextrema
+from matplotlib import pyplot as pl
+
+from job import job
+
+import numpy as np
+
+import sys
+import os
+
+jobs_dir = sys.argv[1]  # The job directories
+job_name = sys.argv[2]  # The generic name for jobs
+export_dir = sys.argv[3]  # The export directory
+datadirname = sys.argv[4]  # Name of data directory
+plotdirname = sys.argv[5]  # Name of plot directory
+
+trajdotlammpstrj = sys.argv[6]  # Trajectories
+testdotout = sys.argv[7]  # LAMMPS print to screen
+depdotin = sys.argv[8]  # Input file
+
+# Loop for each path
+for item in os.walk(jobs_dir):
+
+    path = item[0]
+
+    split = path.split('/')
+
+    # Filter for paths that contain jobs
+    if job_name not in split[-1]:
+        continue
+
+    print(path)
+    run = job(path, export_dir, datadirname, plotdirname)
+
+    run.input_file(depdotin)
+    run.sys(testdotout)
+    run.box(trajdotlammpstrj)
+
+    run.vp()
+
+    print('-'*79)
