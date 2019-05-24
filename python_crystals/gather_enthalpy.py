82fb8dd381df65b26ad30f0a5791a73aa0fc2c6b
leschultz <laneenriqueschultz@gmail.com>
2019-05-24 09:50:58 -0500

    Finished the enthalpy gatherer. Need to compute the enthalpy differences between phases.

--- python_crystals/gather_enthalpy.py
+++ python_crystals/gather_enthalpy.py
@@ -1,51 +1,69 @@
 #!/usr/bin/env python3
 
-'''
-Construct enthalpy dataframe for analysis data.
-'''
-
-from functools import reduce
-
 import pandas as pd
 
 import sys
 import os
 
-jobs_dir = sys.argv[1]  # Directory where run analysis data is stored
-diffusion_file = sys.argv[2]  # Tg file name
-export_dir = sys.argv[3]  # The export directory
+jobs_dir = sys.argv[1]  # The job directories
+export_dir = sys.argv[2]  # The export directory
+crystal_ref_run = sys.argv[3]  # The name of the input file
+logdotlammps = sys.argv[4]  # The name of the log file
 
 # Make export directory
 if not os.path.exists(export_dir):
     os.makedirs(export_dir)
 
-rows = []
+# Loop for each path
+df = []
 for item in os.walk(jobs_dir):
-    path = item[0]
-    files = item[-1]
 
-    # Check if files are present
-    if diffusion_file not in files:
+    path = item[0]
+    files = item[2]
+
+    if not ((crystal_ref_run in files) and (logdotlammps in files)):
         continue
 
+    if crystal_ref_run in files:
+        with open(os.path.join(path, crystal_ref_run)) as f:
+            for line in f:
+                line = line.strip().split(' ')
+                if 'thermo_style' == line[0]:
+                    line = [i for i in line if i != '']
+                    columns = line[2:]
+
+    data = []
+    if logdotlammps in files:
+
+        with open(os.path.join(path, logdotlammps)) as f:
+            for line in f:
+                line = line.strip().split(' ')
+                line = [i for i in line if '' != i]
+
+                if len(line) != len(columns):
+                    continue
+
+                try:
+                    row = [float(i) for i in line]
+                    data.append(row)
+
+                except Exception:
+                    pass
+
     split = path.split('/')
-    system = split[-5]
-    composition = split[-4]
-    tg = float(split[-3])
-    job = split[-2]
 
-    diffusion_path = os.path.join(path, diffusion_file)
+    phase = split[-3]
+    system = split[-2]
+    composition = split[-1]
 
-    diffusion = pd.read_csv(diffusion_path)
-    diffusion['system'] = system
-    diffusion['composition'] = composition
-    diffusion['tg'] = tg
-    diffusion['job'] = job
+    dfdata = pd.DataFrame(data, columns=columns)
+    enthalpy = dfdata['enthalpy'].values[-1]
 
-    rows.append(diffusion)
+    row = [phase, system, composition, enthalpy]
+    df.append(row)
 
-df = pd.concat(rows)
-df = df.reset_index(drop=True)
+df = pd.DataFrame(df)
+df.columns = ['phase', 'system', 'composition', 'enthalpy']
 
-df.to_csv(os.path.join(export_dir, 'diffusion_df.txt'), index=False)
-df.to_html(os.path.join(export_dir, 'diffusion_df.html'), index=False)
+df.to_csv(os.path.join(export_dir, 'enthalpy_df.txt'), index=False)
+df.to_html(os.path.join(export_dir, 'enthalpy_df.html'), index=False)
